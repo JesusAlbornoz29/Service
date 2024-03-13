cb59ed3ee86c4187195f96ba034814cd0eb0a1b6
HP <anomalia23@hotmail.es>
2024-03-13 13:14:20 -0300

    Repaso General de dudas

--- movie-service/src/main/java/com/dh/movieservice/controller/MovieController.java
+++ movie-service/src/main/java/com/dh/movieservice/controller/MovieController.java
@@ -1,3 +1,17 @@
+/* Controller: Contine clases que manejan la
+solicitudes del cliente
+(Se refiere a las peticiones que un cliente (NavegadorWeb, AppMovile, etc) hace a un servidor para obtener datos o realizar acciones
+y
+coordinan la logica de la aplicacion
+(Se refiere a la gestion de las operaciones y procesos que la app debe realizar en respuesta a estas solicitudes)
+.
+
+EJ: En una aplicacion web de comercio electronico, cuando un usuario hace clic en el boton "Agregar al carrito", el navegador envia una solicitud al servidor.
+El controlador correspondiente recibe esta solcitud y puede coordinar la logica de la applicacion
+necesaria para agregar el articulo al carrito del usuario. Esto puede implicar verificar la disponibilidad del producto, actualizar la cantidad en el carrito,
+calcular el total, etc...
+*/
+
 package com.dh.movieservice.controller;
 
 import com.dh.movieservice.model.Movie;
@@ -7,22 +21,28 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/movies")
+@RestController // En lugar de vistas, devuelve datos serializados (JSON O XML) directamente a la respuesta HTTP.
+@RequestMapping("/movies") // Todas las solicitudes seran dirigidas a '/movies'
 public class MovieController {
     private final MovieService movieService;
+    // declaramos movieService que proporciona los servicios relacionados con las peliculas, como buscar peliculas por genero o guardar una nueva pelicula.
 
     public MovieController(MovieService movieService) {
         this.movieService = movieService;
     }
+    // Creamos el constructor MovieController y le inyectamos la dependencia de MovieService para que el controlador interactue con la capa Service
 
-    @GetMapping("/{genre}")
-    ResponseEntity<List<Movie>> getMovieByGenre(@PathVariable String genre) {
+    @GetMapping("/{genre}") // Indica que Se manejaran peticiones GET
+    ResponseEntity<List<Movie>> getMovieByGenre(@PathVariable String genre) { //@PathVariable extrae lo obtenido en @GetMapping("/{genre}") y lo guarda en String genre
         return ResponseEntity.ok().body(movieService.findByGenre(genre));
+        // ResponseEntity> los datos findByGenere(genre) se envuelven dentro de un 'ResponseEntity'
+        // .ok() Indica que la solicitud se completo con exito
+        // body(). Establece el cuerpo de la respuesta contine la lista de peliculas encotradas por genero
     }
 
-    @PostMapping("/save")
+    @PostMapping("/save") // Indica que se manejaran peticiones POST
     ResponseEntity<Movie> saveMovie(@RequestBody Movie movie) {
         return ResponseEntity.ok().body(movieService.save(movie));
+
     }
 }
--- movie-service/src/main/java/com/dh/movieservice/model/Movie.java
+++ movie-service/src/main/java/com/dh/movieservice/model/Movie.java
@@ -1,3 +1,5 @@
+// Modelo o Entidad (Model or Entity)
+
 package com.dh.movieservice.model;
 
 import jakarta.persistence.Entity;
@@ -10,16 +12,19 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
-@Data
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
-public class Movie implements Serializable {
+@Data // @Data viene del Project Lombok genera automaticamente metodos 'toString()','equals()','hashCode()','getters y setters'
+@Entity // @Entity  viene de JPA (Java Persistence API) marca la clase como una entidad.
+@AllArgsConstructor // @AllArgsConstructor viene de Lombok genera automaticamente un constructor que toma como argumetno todos los campos de una clase
+@NoArgsConstructor // @NoArgsConstructor viene de Lombok genera automaticamente un constructor sin argumentos
+public class Movie implements Serializable { // Serializable esta interfaz es necesaria en algunos contextos, como al enviar objetos a traves de la red o al alamacenarlos en archivos.
 
     private static final long serialVersionUID = 1L;
+    // Esta linea declara un campo que es un identificador unico para la versinon serializada de la clase. Se utiliza para asegurar que la version serializada de la clase sea compatible con su version deserializada.
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    // Aqui especificamos como se generan los valores para la clave primaria.
+
     private Long id;
 
     private String name;
