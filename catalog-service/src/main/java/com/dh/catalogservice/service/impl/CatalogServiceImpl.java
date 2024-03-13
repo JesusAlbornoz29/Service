f9fc5f81d90f2d6a5846437210597b7f6e9b881b
HP <anomalia23@hotmail.es>
2024-03-13 15:59:32 -0300

    Seguimos repasando el codigo comentando

--- catalog-service/src/main/java/com/dh/catalogservice/service/impl/CatalogServiceImpl.java
+++ catalog-service/src/main/java/com/dh/catalogservice/service/impl/CatalogServiceImpl.java
@@ -1,3 +1,8 @@
+/*
+Esta carpeta Service/impl> Contiene las implementaciones concretas de las interfaces destinadas en la carpeta de servicio
+Aqui se encuentra la logica ya que en /Service/CatalogoService es una clase abstraca la logica se define aqui
+*/
+
 package com.dh.catalogservice.service.impl;
 
 import com.dh.catalogservice.client.IMovieClient;
@@ -10,16 +15,16 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
-public class CatalogServiceImpl extends CatalogService {
+public class CatalogServiceImpl extends CatalogService { // Aqui practicamente decimos que vamos a heredar los metodos definidos en CatalogoService para darles logica
 
-    @Autowired
+    @Autowired // @Autowired viene de Spring Framework se utiliza para facilitar la inyección de dependencias y reducir la necesidad de crear manualmente instancias de clases dependientes en tu código
     private IMovieClient iMovieClient;
 
     public CatalogServiceImpl(IMovieClient iMovieClient) {
         this.iMovieClient = iMovieClient;
-    }
+    } // Esto es el constructor de CatalogServiceImpl que toma una instancia de IMovieClient, esto quire decir que realizara algunas operaciones relacionada con peliculas a travez de la inyeccion de dependencia
 
-    @Override
+    @Override //se utiliza para asegurar que el metodo sobrescribe correctamente el metodo con el mismo nombre de su calse base
     public Genre findByGenre(String genre) throws GenreNotFoundException {
         ResponseEntity<List<Movie>> response = iMovieClient.getMovieByGenre(genre);
         if (response.getStatusCode().is2xxSuccessful()) {
@@ -27,6 +32,13 @@ public class CatalogServiceImpl extends CatalogService {
         }
         throw new GenreNotFoundException("No se encontró el género: " + genre);
     }
+
+    /*
+    Entendamos el codigo de arriba, este método findByGenre() busca películas por género utilizando un cliente de películas (iMovieClient).
+    Si la respuesta del servidor es exitosa, crea un objeto Genre con la lista de películas obtenidas y lo devuelve.
+    Si la respuesta no es exitosa, lanza una excepción GenreNotFoundException.
+    La anotación @Override garantiza que este método sobrescribe correctamente un método con el mismo nombre de su clase base o interfaz.
+    */
 
     @Override
     public void saveMovie(Movie movie) {
