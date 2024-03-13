6cbf1d9e5787b427bfc6937d1a9b142fdb195690
HP <anomalia23@hotmail.es>
2024-03-13 13:48:36 -0300

    Repasando, comentarios 1.0

--- movie-service/src/main/java/com/dh/movieservice/repository/MovieRepository.java
+++ movie-service/src/main/java/com/dh/movieservice/repository/MovieRepository.java
@@ -1,3 +1,8 @@
+/*
+    Repository> En Java en el conteto de Spring Framework y JPA (Java Persistence API),
+    define la capa de acceso a datos de la aplicacion. Se utiliza para interactuar con la base de datos  o cualquier otra fuente de datos.
+*/
+
 package com.dh.movieservice.repository;
 
 import com.dh.movieservice.model.Movie;
@@ -6,7 +11,9 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, Long> {
+@Repository // Spring Data JPA marca la interfaz como un componente de repositorio
+public interface MovieRepository extends JpaRepository<Movie, Long> // JpaRespository es una interfaz proporcionada por Spring Data JPA que proporciona
+    // metodos CRUD basicos
+{
     List<Movie> findByGenre(String genre);
 }
