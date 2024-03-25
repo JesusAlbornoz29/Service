3251670fd2647a8951921dc85163559f312168cc
HP <anomalia23@hotmail.es>
2024-03-25 13:25:19 -0300

    Agregando Zipkin (img POSTMAN) y micrometer a Todos los microservicios

--- catalog-service/src/main/java/com/dh/catalogservice/Controller/CatalogController.java
+++ catalog-service/src/main/java/com/dh/catalogservice/Controller/CatalogController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/catalog")
@@ -20,29 +21,38 @@ public class CatalogController {
     @Autowired
     private ISerieClient iSerieClient;
 
+    private static java.util.logging.Logger log = Logger.getLogger(CatalogController.class.getName());
+
+
+
     @GetMapping("/{genre}")
     public ResponseEntity<List<Movie>> getCatalogByGenre(@PathVariable String genre) {
+        log.info("Se encontro la siguiente lista de peliculas por genero");
         return iMovieClient.getMovieByGenre(genre);
     }
 
     @PostMapping("/movies/save")
     public ResponseEntity<Movie> saveMovie(@RequestBody Movie movie) {
+        log.info("Se guardo la pelicula correctamente");
         return iMovieClient.saveMovie(movie);
     }
 
     // Series
     @GetMapping("/series")
     public List<Serie> getAllSeries() {
+        log.info("Se encontro la siguiente lista de Series");
         return iSerieClient.getAll();
     }
 
     @GetMapping("/series/{genre}")
     public List<Serie> getSeriesByGenre(@PathVariable String genre) {
+        log.info("Se encontro la siguiente lista de Series por genero");
         return iSerieClient.getSerieByGenre(genre);
     }
 
     @PostMapping("/series/save")
     public String createSerie(@RequestBody Serie serie) {
+        log.info("Se guardo la serie correctamente");
         return iSerieClient.crearSerie(serie);
     }
 
